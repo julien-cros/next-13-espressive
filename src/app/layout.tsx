eeab116cc692ff9d7e022f87b70cb5e858476ff1
Julien Cros <julien.cros86@gmail.com>
2024-01-20 15:51:06 +0100

    feat: add favicon + update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,25 @@ const poppins = Poppins({
 export const metadata: Metadata = {
 	metadataBase: new URL("https://espressive.fr"),
   title: "Espressive",
-  description: "A blog for the world of espresso",
+  description: "A blog about the world of espresso",
+	openGraph: {
+		type: "website",
+		locale: "en_US",
+		url: "https://espressive.fr",
+		images: [
+			{
+				url: "https://espressive.fr/images/coffee.jpg",
+				width: 980,
+				height: 980,
+				alt: "Espressive",
+			},
+		],
+	},
+	twitter: {
+		title: "Espressive",
+		site: "@espressive",
+		card: "summary_large_image",
+	},
 };
 
 export default function RootLayout({
