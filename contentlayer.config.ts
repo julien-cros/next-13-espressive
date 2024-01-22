cd8afbc38b1bc255c58da46e7358e2c03de518b7
Julien Cros <julien.cros86@gmail.com>
2024-01-22 15:10:12 +0100

    feat: add tags keyword metadata

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,36 +1,37 @@
 import {
-  defineDocumentType,
-  makeSource,
+	defineDocumentType,
+	makeSource,
 } from "@contentlayer/source-files";
 import remarkGfm from "remark-gfm";
 
 export const Post = defineDocumentType(() => ({
-  name: "Post",
-  filePathPattern: "**/*.mdx",
-  contentType: "mdx",
-  fields: {
-    title: { type: "string", required: true },
-    createdAt: { type: "string", required: true },
-    updatedAt: { type: "string", required: true },
+	name: "Post",
+	filePathPattern: "**/*.mdx",
+	contentType: "mdx",
+	fields: {
+		title: { type: "string", required: true },
+		createdAt: { type: "string", required: true },
+		updatedAt: { type: "string", required: true },
 		description: { type: "string", required: true },
-  },
-  computedFields: {
-    url: {
-      type: "string",
-      resolve: (post) => `/post/${post._raw.flattenedPath}`,
-    },
-	slug: {
-		type: "string",
-		resolve: (post) => post._raw.flattenedPath,
+		tags: { type: "list", of: { type: "string" }, required: true },
 	},
-  },
+	computedFields: {
+		url: {
+			type: "string",
+			resolve: (post) => `/post/${post._raw.flattenedPath}`,
+		},
+		slug: {
+			type: "string",
+			resolve: (post) => post._raw.flattenedPath,
+		},
+	},
 }));
 
 
 export default makeSource({
 	contentDirPath: 'posts',
-  documentTypes: [Post],
-  mdx: {
+	documentTypes: [Post],
+	mdx: {
 		remarkPlugins: [remarkGfm],
-  },
+	},
 });
